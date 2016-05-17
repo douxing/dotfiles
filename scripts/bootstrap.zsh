a8c93e6371b2824ba5034731e112ad4a61e6e2f3
dx <douxing@ceasia.com.cn>
2016-05-17 20:59:38 +0800

    first step

---
+++ scripts/bootstrap.zsh
@@ -0,0 +1,25 @@
+#!/usr/bin/zsh
+#
+# bootstrap using zsh
+
+PWDP=$(pwd -P)
+
+link_file () {
+    local src=$1dst=$2
+
+    ln -s "$1" "$2"
+}
+
+symbolize_all () {
+    printf "pwd -p $PWDP"
+
+    for src in $(find -H "$PWDP" -maxdepth 2 -name '*.symlink')
+    do
+        dst="$HOME/.$(basename "${src%.*}")"
+	link_file "$src" "$dst"
+
+	printf "$src $dst"
+    done
+}
+
+symbolize_all
