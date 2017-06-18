961a6562cb8a6bde860ccce60f624426acb46c83
douxing <douxing1983@163.com>
2017-06-18 11:28:08 +0000

    bug fix for symlink

--- scripts/bootstrap.zsh
+++ scripts/bootstrap.zsh
@@ -5,11 +5,14 @@
 PWDP=$(pwd -P)
 
 link_file () {
-    local src=$1dst=$2
+    local src=$1 dst=$2
 
-    ln -s "$1" "$2"
+    ln -s "$src" "$dst"
 }
 
+# for file extension and base
+# @see https://stackoverflow.com/questions/965053/extract-filename-and-extension-in-bash
+# @see https://www.gnu.org/software/bash/manual/html_node/Shell-Parameter-Expansion.html#Shell-Parameter-Expansion
 symbolize_all () {
     for src in $(find -H "$PWDP" -maxdepth 2 -name '*.symlink')
     do
