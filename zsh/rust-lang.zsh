93079afeec99f378a08fcf4f8393dd4583f5b0d3
douxing <douxing@zccp.com>
2019-02-14 11:03:06 +0800

    add mirror in ustc for rust config

--- zsh/rust-lang.zsh
+++ zsh/rust-lang.zsh
@@ -1,3 +1,7 @@
 if [ -d $HOME/.cargo/bin ]; then
+    # @see https://lug.ustc.edu.cn/wiki/mirrors/help/rust-static
+    export RUSTUP_DIST_SERVER=https://mirrors.ustc.edu.cn/rust-static
+    export RUSTUP_UPDATE_ROOT=https://mirrors.ustc.edu.cn/rust-static/rustup
+
     export PATH=$HOME/.cargo/bin:$PATH
 fi
